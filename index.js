1ca59d1130b6acc50bbae1dc8c3b03e0bebe4a9a
Rex Lundstrom <rexlundstrom@gmail.com>
2021-10-13 13:55:49 -0400

    clean up the count truthy function

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function countTruthy(array) {
     let counter = 0;
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] !== false && array[i] !== null && array[i] !== undefined
-            && array[i] !== 0 && array[i] !== '') {
+    for(let value of array) {
+        if(value)
             counter++;
         }
-    }
     return counter;
 }
 const testArray = [3, 4, 'fred', true, null, 'tacos', '', 0, 70]
