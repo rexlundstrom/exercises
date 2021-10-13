3d04b2fa20fd1a81f0bfa684db51f49339de283d
Rex Lundstrom <rexlundstrom@gmail.com>
2021-10-13 13:51:33 -0400

    build ugly but functional truthy counter

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const showNumbers = limit => {
-    for(let i = 0; i <= limit; i++){
-        if(i % 2 === 0) {
-            console.log(i + ' "EVEN"')
-        } 
-        else {
-            console.log(i + ' "ODD"')
+function countTruthy(array) {
+    let counter = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] !== false && array[i] !== null && array[i] !== undefined
+            && array[i] !== 0 && array[i] !== '') {
+            counter++;
         }
     }
+    return counter;
 }
-
-showNumbers(17);
+const testArray = [3, 4, 'fred', true, null, 'tacos', '', 0, 70]
+console.log(countTruthy(testArray));
