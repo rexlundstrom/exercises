2fd72a22008583393602c6d6b6c7bd171528cfa0
Rex Lundstrom <rexlundstrom@gmail.com>
2021-10-14 08:19:01 -0400

    make a compact grade calculator

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-console.log(funSum(5));
+const marks = [90, 85, 30];
 
-function funSum(limit) {
-    let counter = 0;
+const calculateGrade = marks => {
+    let sumOfMarks = 0;
+    for (const mark of marks) {
+        sumOfMarks += mark;
+    }
+    let averageGrade = sumOfMarks / marks.length;
+    if (averageGrade < 60) return 'F';
+    else if (averageGrade < 70) return 'D';
+    else if (averageGrade < 80) return 'C';
+    else if (averageGrade < 90) return 'B';
+    else if (averageGrade <= 100) return 'A';
+    else if (averageGrade > 100) return 'A+';
 
-    for(let i = 0; i <= limit; i++)
-        if(i % 3 === 0 || i % 5 === 0)
-            counter = counter + i;
-
-    return counter;
 }
+
+console.log(calculateGrade(marks));
