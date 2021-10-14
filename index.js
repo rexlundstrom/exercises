b51f628b803169d98796d151798781b2cff19665
Rex Lundstrom <rexlundstrom@gmail.com>
2021-10-14 09:20:56 -0400

    show stars function

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const marks = [90, 85, 30];
 
-const calculateGrade = marks => {
-    let sumOfMarks = 0;
-    for (const mark of marks) {
-        sumOfMarks += mark;
+showStars(10);
+
+function showStars(rows) {
+    let stars = '';
+    for(let i = 0; i < rows; i++){
+        stars += '*'
+        console.log(stars);
     }
-    let averageGrade = sumOfMarks / marks.length;
-    if (averageGrade < 60) return 'F';
-    else if (averageGrade < 70) return 'D';
-    else if (averageGrade < 80) return 'C';
-    else if (averageGrade < 90) return 'B';
-    else if (averageGrade <= 100) return 'A';
-    else if (averageGrade > 100) return 'A+';
-
+    
 }
 
-console.log(calculateGrade(marks));
