44f24ba47d5b1327f6cb631637976f673bc7ff1f
Rex Lundstrom <rexlundstrom@gmail.com>
2021-10-14 21:14:33 -0400

    make an IIFE for practice

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
+let firstName = 'Tommy';
 
-showPrimes(100);
+(function() {
+    firstName = 'Billy';
+    console.log(firstName);
+})
 
-function showPrimes(limit) {
-    for(let i = 2; i <= limit; i++){
-
-        let isPrime = true;
-        for(let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) console.log(i);
-    }
-}
-
+console.log(firstName);
