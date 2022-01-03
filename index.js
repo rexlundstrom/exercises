3271a6601c1f59a2d7acadfec45214a573a24dd5
Rex Lundstrom <rexlundstrom@gmail.com>
2022-01-03 07:39:23 -0500

    index of dog function complete

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let firstName = 'Tommy';
+const numbers = [3, 4];
 
-(function() {
-    firstName = 'Billy';
-    console.log(firstName);
-})
+numbers.push(5, 6);
 
-console.log(firstName);
+
+numbers.unshift(1, 2);
+
+numbers.splice(2, 2, 'tree', 'for')
+
+const dogs = [
+    { name: 'Barky', breed: 'collie'},
+    { name: 'Woofy', breed: 'shephard'},
+    { name: 'Howler', breed: 'beagle'},
+    { name: 'Ruffy', breed: 'pomsky'}
+];
+
+const indexOfDog = dogs.findIndex(function(dog) {
+  return dog.name === 'Howler';
+});
+
+console.log(indexOfDog);
